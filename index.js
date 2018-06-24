2b3cdbc7e6f718fd4b477799322d18d108aa6bdb
Farida <faridah.absi@gmail.com>
2018-06-24 19:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function sayHiToGrandma(string){
   if (string === 'hello'){
    return "I can't hear you!"}
   
-  else if (string === 'hello'){
+  else if (string === 'HELLO'){
    return "I can't hear you!"}
 }
